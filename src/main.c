ccf81b0ab521c86cc02d03658072574e6b360cb1
Lloyd Atkinson <lloydjatkinson@gmail.com>
2020-01-07 00:10:56 +0000

    Add code to flash 4 LED's on PORTD

--- src/main.c
+++ src/main.c
@@ -1,22 +1,28 @@
-/**
- * Copyright (C) PlatformIO <contact@platformio.org>
- * See LICENSE for details.
- */
-
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
     // make the LED pin an output for PORTB5
-    DDRB = 1 << 5;
+    // DDRB = (1 << 5) | (1 << 2);
+    // DDRB = 0x00;
+
+    DDRB = (1 << DDB5);
+    DDRD = (1 << DDD2) | (1 << DDD3) | (1 << DDD4) | (1 << DDD5);
 
     while (1)
     {
-        _delay_ms(500);
+        _delay_ms(200);
 
         // toggle the LED
-        PORTB ^= 1 << 5;
+        PORTB ^= (1 << 5);
+        // PORTB = 0xFF;
+
+        // PORTB ^= (1 << PB5) | (1 << PB2);
+        // PORTB ^= (1 << PB2);
+
+        PORTD ^= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);
+        
     }
 
     return 0;
